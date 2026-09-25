6c404ba1d85144f71130aaba61d934bcd3510bea
Thiago Reis Trindade <trindade.reisthiago@gmail.com>
2026-09-25 15:20:28 -0300

    Tentando respeitar o complice

--- bronze_forno/campanha_semana.py
+++ bronze_forno/campanha_semana.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-caminho = r"Q:\GROUPS\BR_SC_JGS_WMO_METALURGICO_II\07 - FUSÃO\14 - ACOMPANHAMENTO CONFECÇÃO FORNO 30T\2025\Campanhas do Ano - Copia.xlsx"
+caminho = r"bronze_forno/dados_campanhas_fornos_2024.xlsx"
 
 df_campanhas = pd.read_excel(caminho)
 
@@ -11,19 +11,15 @@ Colunas
 ['Unnamed: 0', 'Forno', 'Capacidade (ton)', 'Campanha 2024', 'Semana da campanha', 'Data', 'Semana do Ano', 'Quantidade de Refratário']
 """
 
-#print(df_campanhas.head())
+print(df_campanhas.head())
 
-df_campanha_silver = df_campanhas[['Forno', 'Campanha 2024', 'Semana da campanha', 'Semana do Ano']]
+#df_campanha_silver = df_campanhas[['Forno', 'Campanha 2024', 'Semana da campanha', 'Semana do Ano']]
 
-nome_colunas = {
-    'Campanha 2024' : 'Campanha'
-}
 
-df_campanhas.replace(nome_colunas)
 
-print(df_campanha_silver.head())
+#print(df_campanha_silver.head())
 
-df_campanha_silver.to_excel("silver_forno/campanhas_do_forno.xlsx", index= False)
+#df_campanha_silver.to_excel("silver_forno/campanhas_do_forno.xlsx", index= False)
 
 #print("\n")
 #print("Shape", df_campanhas.shape)
